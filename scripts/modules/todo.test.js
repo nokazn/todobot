88a6d4108874c75843106c6cfafb80dd91d51960
nokazn <nokazn@gmail.com>
2020-08-21 22:25:27 +0900

    modify test

--- scripts/modules/todo.test.js
+++ scripts/modules/todo.test.js
@@ -5,17 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const todo_1 = __importDefault(require("./todo"));
 const assert_1 = __importDefault(require("assert"));
-// todo と list のテスト
-todo_1.default.todo('ノートを買う');
-todo_1.default.todo('鉛筆を買う');
-assert_1.default.deepEqual(todo_1.default.list(), ['ノートを買う', '鉛筆を買う']);
-// done と donelist のテスト
-todo_1.default.done('鉛筆を買う');
-assert_1.default.deepEqual(todo_1.default.list(), ['ノートを買う']);
-assert_1.default.deepEqual(todo_1.default.doneList(), ['鉛筆を買う']);
-// del のテスト
-todo_1.default.del('ノートを買う');
-todo_1.default.del('鉛筆を買う');
-assert_1.default.deepEqual(todo_1.default.list(), []);
-assert_1.default.deepEqual(todo_1.default.doneList(), []);
-console.log('Test is done.');
+const fs_1 = __importDefault(require("fs"));
+fs_1.default.unlink('./tasks.json', () => {
+    // todo と list のテスト
+    todo_1.default.todo('ノートを買う');
+    todo_1.default.todo('鉛筆を買う');
+    assert_1.default.deepEqual(todo_1.default.list(), ['ノートを買う', '鉛筆を買う']);
+    // done と donelist のテスト
+    todo_1.default.done('鉛筆を買う');
+    assert_1.default.deepEqual(todo_1.default.list(), ['ノートを買う']);
+    assert_1.default.deepEqual(todo_1.default.doneList(), ['鉛筆を買う']);
+    // del のテスト
+    todo_1.default.del('ノートを買う');
+    todo_1.default.del('鉛筆を買う');
+    assert_1.default.deepEqual(todo_1.default.list(), []);
+    assert_1.default.deepEqual(todo_1.default.doneList(), []);
+    console.log('Test is done.');
+});
--- src/modules/todo.test.ts
+++ src/modules/todo.test.ts
@@ -1,20 +1,23 @@
 import todo from './todo';
 import assert from 'assert';
+import fs from 'fs';
 
-// todo と list のテスト
-todo.todo('ノートを買う');
-todo.todo('鉛筆を買う');
-assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
+fs.unlink('./tasks.json', () => {
+  // todo と list のテスト
+  todo.todo('ノートを買う');
+  todo.todo('鉛筆を買う');
+  assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
 
-// done と donelist のテスト
-todo.done('鉛筆を買う');
-assert.deepEqual(todo.list(), ['ノートを買う']);
-assert.deepEqual(todo.doneList(), ['鉛筆を買う']);
+  // done と donelist のテスト
+  todo.done('鉛筆を買う');
+  assert.deepEqual(todo.list(), ['ノートを買う']);
+  assert.deepEqual(todo.doneList(), ['鉛筆を買う']);
 
-// del のテスト
-todo.del('ノートを買う');
-todo.del('鉛筆を買う');
-assert.deepEqual(todo.list(), []);
-assert.deepEqual(todo.doneList(), []);
+  // del のテスト
+  todo.del('ノートを買う');
+  todo.del('鉛筆を買う');
+  assert.deepEqual(todo.list(), []);
+  assert.deepEqual(todo.doneList(), []);
 
-console.log('Test is done.');
+  console.log('Test is done.');
+});
